90eecbb6b583f5540fc26e695150960f1b6ffade
santoshbt <bt.santosh01@gmail.com>
2018-07-12 17:01:31 +0530

    fixed id for show user

--- src/actions/index.js
+++ src/actions/index.js
@@ -57,7 +57,7 @@ export function fetchPost(id) {
 }
 
 export function fetchUser(id){
-  const request = axios.get(`${ROOT_URL}/users?id=${id}`);
+  const request = axios.get(`${ROOT_URL}/users/${id}`);
   // return {
   //   type: FETCH_USER,
   //   payload: reque st
